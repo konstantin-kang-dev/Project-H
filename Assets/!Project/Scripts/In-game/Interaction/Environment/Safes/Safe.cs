ac5bb81e1ce3a90849e8a50eac0f254af01e99a3
Konstantin Kang <konstantin.kang.dev@gmail.com>
2026-03-04 18:14:02 +0500

    Created Safe logic, Added keys for all existing locked rooms, Objectives manager fixes

---
+++ Assets/!Project/Scripts/In-game/Interaction/Environment/Safes/Safe.cs
@@ -0,0 +1,47 @@
+using FishNet.Object.Synchronizing;
+using UnityEngine;
+
+public class Safe : BasicInteractable
+{
+    [SerializeField] ObjectiveType _requiredObjectiveToCollect;
+
+    [SerializeField] DoorVisuals _doorVisuals;
+
+    readonly SyncVar<bool> _isLocked = new SyncVar<bool>();
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        RequirementsHintText = $"Collect all {ProjectUtils.CamelCaseToSpaced(_requiredObjectiveToCollect.ToString())} (s)";
+    }
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        _isLocked.Value = true;
+
+        ObjectivesManager.Instance.OnObjectiveCompleted += HandleObjectiveCompleted;
+    }
+
+    public override void Interact(IPickable pickableInHand)
+    {
+
+    }
+
+    void HandleObjectiveCompleted(ObjectiveType type)
+    {
+        if(type == _requiredObjectiveToCollect)
+        {
+            _interactState.Value = true;
+            Debug.Log($"[Safe] Unlocked! Objective collected: {type}");
+        }
+    }
+
+    public override void SetAppearance(bool value)
+    {
+        base.SetAppearance(value);
+
+        _doorVisuals.SetState(value);
+    }
+}
